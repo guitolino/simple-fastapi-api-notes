68bb94cdf8206da2ffdd5ee4e8ee56d535ebd1dd
guitolino <migantp25@gmail.com>
2025-12-28 15:52:00 -0500

    feat: set up settings
    manages the environment variables inside te .env file

---
+++ backend/settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+# !update the .env.example file
+
+class Settings(BaseSettings):
+    database_url: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
